303e15e173dbf6b83a8ddc27385b30e25d3462a6
fabzcode <fabloz12g@gmail.com>
2024-05-31 12:34:12 -0700

    added a few codes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-ECHO is on.
+<script>
+
+const App =() =>{
+    const [loading, setloading] =(0);
+    const[name, setname] = (0)
+          return(
+    <>
+    
+    </>
+        );
+};
+    
+    export default App;
+
+
+
+
+    </script>
